c7d9a64465b60fca8cbcb4c477cc09a91b6cfbfc
adarsh <adarshurmaliya@gmail.com>
2022-03-04 18:01:47 +0530

    issues fixed

--- Back-end/index.js
+++ Back-end/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 connectToMongo();
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 // to enable to sent json file
